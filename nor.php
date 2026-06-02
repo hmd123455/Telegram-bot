2fe08445351705fd2c89b6bb6201b74ab6717674
hmd123455 <mdhmd844@gmail.com>
2026-06-02 12:16:18 +0300

    Update print statement to say 'Goodbye World'

--- nor.php
+++ nor.php
@@ -7,18 +7,15 @@ $bot = [];
 $admins = $bot['admins'];
 $domin = $_SERVER['HTTP_HOST'];
 
-function bot($method, $datas = []) {
-    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
+function bot($method, $datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $res = curl_exec($ch);
-    if (curl_error($ch)) {
-        var_dump(curl_error($ch));
-    } else {
-        return json_decode($res);
-    }
+    return json_decode($res);
 }
 
 function callAPI($action, $channel_id, $user_id = null, $number = 1) {
